612d443fbfe0f51b9ab6f85f637ef666197cd3ad
cph <stegben.benjamin@gmail.com>
2017-03-14 19:31:17 +0800

    config eslint and fix lints

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    extends: 'airbnb',
+    plugins: [
+      'react',
+      'jsx-a11y',
+      'import',
+    ],
+    rules: {
+      'react/jsx-filename-extension': 0,
+      'react/prefer-stateless-function': 1,
+    },
+    env: {
+      'browser': true,
+      'jest': true,
+    }
+};
