c5b63a422d1b51fc7bd8bfa9939783e77473337a
cph <stegben.benjamin@gmail.com>
2017-03-18 14:19:36 +0800

    init TodoApp state

--- src/App.js
+++ src/App.js
@@ -2,9 +2,34 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      todoLists: [
+        {
+          title: 'sample todo list 1',
+          todos: [
+            { content: 'todo 1-1', done: true },
+            { content: 'todo 1-2', done: false },
+            { content: 'todo 1-3', done: true },
+          ],
+        },
+        {
+          title: 'sample todo list 1',
+          todos: [
+            { content: 'todo 1-1', done: true },
+            { content: 'todo 1-2', done: false },
+            { content: 'todo 1-3', done: true },
+          ],
+        },
+      ],
+    };
+  }
+
   render() {
     return (
-      <div className="">
+      <div>
+        <h1 className="App">Todo Lists</h1>
       </div>
     );
   }
